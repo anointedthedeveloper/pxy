ee3b24efc0dcfe19f9f41403bdd40e776447d8eb
anointedthedeveloper <anointedthedeveloper@gmail.com>
2026-07-04 15:06:14 +0100

    Revert to CommonJS module.exports

--- api/index.js
+++ api/index.js
@@ -63,7 +63,7 @@ function checkAccessCode(req) {
 }
 
 // Vercel serverless handler
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
     // Enable CORS
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -150,4 +150,4 @@ export default async function handler(req, res) {
     } catch (error) {
         res.status(500).json({ error: 'Internal Server Error', message: error.message });
     }
-}
+};
